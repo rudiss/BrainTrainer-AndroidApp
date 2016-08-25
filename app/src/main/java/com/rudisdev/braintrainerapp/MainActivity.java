7955af103123c7e6d3c85a7020f83b86f585e305
rudis <rudislpp@gmail.com>
2016-08-25 19:59:03 -0300

    start button

--- app/src/main/java/com/rudisdev/braintrainerapp/MainActivity.java
+++ app/src/main/java/com/rudisdev/braintrainerapp/MainActivity.java
@@ -2,8 +2,19 @@ package com.rudisdev.braintrainerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+     Button startButton;
+
+    public void start(View view){
+
+        startButton.setVisibility(View.INVISIBLE);
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
