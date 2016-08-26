f2a7132fe43d4d246ef04474912374aabd8dfec2
rudis <rudislpp@gmail.com>
2016-08-25 21:34:54 -0300

    add main logic

--- app/src/main/java/com/rudisdev/braintrainerapp/MainActivity.java
+++ app/src/main/java/com/rudisdev/braintrainerapp/MainActivity.java
@@ -6,16 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
+    ArrayList<Integer> answers;
+    int locationOFCorrectAnswer;
 
-    public void start(View view){
+    public void start(View view) {
 
         startButton.setVisibility(View.INVISIBLE);
-
 
 
     }
@@ -25,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startButton = (Button)findViewById(R.id.startButton);
-        TextView sumTextView = (TextView)findViewById(R.id.sumTextView);
+        startButton = (Button) findViewById(R.id.startButton);
+        TextView sumTextView = (TextView) findViewById(R.id.sumTextView);
 
         Random rand = new Random();
 
@@ -34,5 +36,30 @@ public class MainActivity extends AppCompatActivity {
         int b = rand.nextInt(21);
 
         sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+        locationOFCorrectAnswer = rand.nextInt(4);
+
+        int incorrectAnswer;
+
+        for (int i = 0; i < 4:i++){
+
+            if (i == locationOFCorrectAnswer) {
+                answers.add(a + b);
+
+            } else {
+
+                incorrectAnswer = rand.nextInt(41);
+
+                while (incorrectAnswer == a + b) {
+
+                    incorrectAnswer = rand.nextInt(41);
+
+
+                }
+
+                answers.add(rand.nextInt(41));
+
+            }
+        }
     }
 }
