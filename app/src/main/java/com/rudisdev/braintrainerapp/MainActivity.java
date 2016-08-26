00e9361792e10d546782b9397812e6709f31ee2c
rudis <rudislpp@gmail.com>
2016-08-25 21:29:14 -0300

    main layout ok

--- app/src/main/java/com/rudisdev/braintrainerapp/MainActivity.java
+++ app/src/main/java/com/rudisdev/braintrainerapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         startButton.setVisibility(View.INVISIBLE);
 
 
+
     }
 
     @Override
@@ -22,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startButton = (Button)findViewById(R.id.startButton);
+        TextView sumTextView = (TextView)findViewById(R.id.sumTextView);
+
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
     }
 }
